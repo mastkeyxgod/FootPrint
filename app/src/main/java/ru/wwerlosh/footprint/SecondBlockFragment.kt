63853f15ff7f9823ea19515bf1ef99e2a62b136c
wwerlosh <igor.shverlo@yandex.ru>
2023-08-08 21:13:51 +0300

    Добавлены спиннеры и поля в первом блоке калькулятора

--- app/src/main/java/ru/wwerlosh/footprint/SecondBlockFragment.kt
+++ app/src/main/java/ru/wwerlosh/footprint/SecondBlockFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 
 class SecondBlockFragment : Fragment() {
@@ -16,5 +19,36 @@ class SecondBlockFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val spinner: Spinner = view.findViewById(R.id.carTypeSpinner)
+        spinner.setBackgroundResource(R.drawable.spinner_border)
+        val types = arrayOf(
+            "Выберите тип автомобиля",
+            "Седан",
+            "Пикап",
+            "Хэтчбек",
+            "Универсал",
+            "Лифтбек",
+            "Минивен",
+            "Купе",
+            "Кроссовер",
+            "Мотоцикл"
+        )
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, types)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedColor = types[position]
+                if (selectedColor == "Выберите тип автомобиля") {
+                    // Это специальный элемент с текстом-подсказкой, игнорируем выбор
+                } else {
+                    // Обработка выбора другого элемента
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Ничего не делаем, так как это обработчик события "ничего не выбрано"
+            }
+        }
     }
 }
